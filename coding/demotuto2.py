035568cf0db5055717e59ac71714fe8386112df1
FaizAhamed2109 <faizahamed2109@gmail.com>
2023-09-13 19:30:27 +0400

    New has been file

--- coding/demotuto2.py
+++ coding/demotuto2.py
@@ -13,7 +13,7 @@ turta.color("white") #make turta color white as default colour is black
 turta.pensize(4) #setting the width of the turta to size 4
 turta.pencolor("white") #changing the turta colour
 turta.speed(1) #setting the speed of the cursor movement from 0 to 200 (1-slowest) & (10-fastest)
-turta.shape("turtle") # setting the shape of the turtle
+turta.shape("triangle") # setting the shape of the turtle
 
 
 turta.setheading(0) # making the turtle to start at an initial direction
@@ -32,28 +32,28 @@ turta.forward(200)
 turta.right(90) #Used for turning the cursor clockwise
 turta.forward(200)
 
-turta.left(90) #Used for turning the cursor anit-clockwise
-turta.forward(200)
+# turta.left(90) #Used for turning the cursor anit-clockwise
+# turta.forward(200)
 
-turta.right(90) #Used for turning the cursor clockwise
-turta.forward(200)
+# turta.right(90) #Used for turning the cursor clockwise
+# turta.forward(200)
 
-turta.right(90) #Used for turning the cursor clockwise
-turta.forward(200)
+# turta.right(90) #Used for turning the cursor clockwise
+# turta.forward(200)
 
-# turta.pendown() # Used for not making the pen create a mark
-turta.right(90) #Used for turning the cursor clockwise
-turta.forward(200)
+# # turta.pendown() # Used for not making the pen create a mark
+# turta.right(90) #Used for turning the cursor clockwise
+# turta.forward(200)
 
 # turta.goto(100, 100)
 # turta.forward(100)
 
-turta.hideturtle() # used for hiding the turtle cursor
+# turta.hideturtle() # used for hiding the turtle cursor
 turta.circle(20)
 turta.dot(20)
 
 turta.begin_fill()
-turta.forward(10)
+turta.forward(100)
 
 turta.circle(20)
 turta.fillcolor("red")
@@ -62,4 +62,3 @@ turta.end_fill()
 turta.home() # making the turtle back to the center point (home)
 
 turtle.done() # This can be also written as win.exitonclick() OR win.bye()
-#
